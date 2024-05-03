4376108d9288dd7abdc1e6c464643709e7e0acd4
Pallavi <pallavijain777@gmail.com>
2024-05-03 14:37:43 +0530

    Did imports for home page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,10 +2,15 @@ import React from "react";
 import NavigationBar from "../../components/other/navigation bar/NavigationBar";
 import EasyToUse from "../../assets/easy to use.jpg";
 import QuickResponses from "../../assets/quick responses.jpg";
+import FreeOfCost from "../../assets/free of cost.jpg";
+import FrequentUpdates from "../../assets/frequent updates.jpg";
+import OpenForAll from "../../assets/open for all.jpg";
+import Responsiveness from "../../assets/responsiveness.jpg";
+import { Link } from "react-router-dom";
 
 
 function Home() {
     return (
-
+        passss
     )
 }
