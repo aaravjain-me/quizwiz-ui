86f60df150d8b226013c614588a22fd914b2b6b6
Pallavi <pallavijain777@gmail.com>
2024-05-07 13:55:06 +0530

    Deletions compleate

--- src/App.js
+++ src/App.js
@@ -1,31 +1,8 @@
-import './pages/r/RegistrationForm.css';
 import React from 'react';
-import Light from './assets/light-bulb.jpg';
-import { Link } from 'react-router-dom';
-import InformationNote from './components/InformationNote';
 
 
 function App() {
-  return (
-    <>
-      <img src={Light} alt='light bulb' />{/*make the imported image appear*/}
-      <InformationNote />
-      <div className='registration'>
-        <form>
-          <div>
-            <label htmlFor='passwords'>Enter your password:</label>
-            <br />
-            <input type='password' name='passwords' id='passwords' />
-          </div>
-          <Link to='./pages/about/About'>Know more...</Link>{/*link to that page*/}
-          <br />
-          <button type='submit'>Login</button>
-        </form>
-        <p>Didn't register for quizwiz</p>
-        <Link to='./pages/r/RegistrationForm'>Register now</Link>
-      </div>
-    </>
-  )
+  
 }
 
 export default App;
--- src/components/InformationNote.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function InformationNote() {
-    return (
-        <p>Note:Registration for this is compleately free</p>
-    )
-}
-
-export default InformationNote;
