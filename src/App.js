382e7037790d0ddd8013b02d549650d4e1eb7817
Pallavi <pallavijain777@gmail.com>
2024-05-10 18:36:01 +0530

    Completed the deletions in NaviationBar.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import NavigationBar from './components/navigation bar/NavigationBar';
 
 
 function App() {
-  
+  return (
+    <NavigationBar />
+  )
 }
 
 export default App;
