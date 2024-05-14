51f015860cd1aa1939c526ab455fd24b8928c22a
Pallavi <pallavijain777@gmail.com>
2024-05-14 13:04:50 +0530

    Deletions compleate

--- src/components/information note/InformationNote.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function InformationNote() {
-    return (
-        <p>Note: Registration for quizwiz is compleately free</p>
-    )
-}
-
-export default InformationNote;
