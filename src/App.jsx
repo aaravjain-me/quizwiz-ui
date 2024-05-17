e4c14dd7d9a7b7437c29af1246ed29502759cf2f
aaravjain-me <aaravjain.me@gmail.com>
2024-05-17 19:02:58 +0530

    Login.jsx compleate

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,20 @@ import Quiz from './components/quiz/class4/QuizGKClassF';
 import Result from './components/Result';
 import quizwizLogo from './assets/logo.webp';
 import NavigationBar from './components/NavigationBar';
-
+import Login from './components/Login';
 import './App.css';
 
 const App = () => {
   return (
     <div className="app-container">
       <Routes>
-        <Route path="/" element={ <NavigationBar /> } /> 
+        <Route path='/' element={ <Login /> } />
+        <Route path="/home" element={
+          <>
+            <img src={quizwizLogo} alt="QuizWiz Logo" className="logo" /> 
+            <NavigationBar />
+          </> 
+        } /> 
         <Route path="/quiz" element={
           <div className="content-wrapper">
             <img src={quizwizLogo} alt="QuizWiz Logo" className="logo" />
